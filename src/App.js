02d2543445d140c46c2e7cac990efd9236c64812
Minhazur Rahman <minhazur.rony@gmail.com>
2017-12-10 22:40:43 +0600

    Add initial functionality

--- src/App.js
+++ src/App.js
@@ -1,18 +1,92 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import { Navbar, NavItem, Nav, Grid, Row, Col } from "react-bootstrap";
+
+const PLACES = [
+  { name: "Palo Alto", zip: "94303" },
+  { name: "San Jose", zip: "94088" },
+  { name: "Santa Cruz", zip: "95062" },
+  { name: "Honolulu", zip: "96803" }
+];
+
+class WeatherDisplay extends Component {
+  constructor() {
+    super();
+    this.state = {
+      weatherData: null
+    };
+  }
+  componentDidMount() {
+    const zip = this.props.zip;
+    const URL =
+      "https://api.apixu.com/v1/current.json?key=13771a2f2737470b976142108170912&q=" +
+      zip;
+    fetch(URL)
+      .then(res => res.json())
+      .then(json => {
+        this.setState({ weatherData: json });
+      });
+  }
+  render() {
+    let weatherData = this.state.weatherData;
+    if (!weatherData) return <div>Loading</div>;
+    // return <div>{JSON.stringify(weatherData)}</div>;
+
+    return (
+      <div>
+        <h1>
+          {weatherData.current.condition.text} in {weatherData.location.name}
+          <img src={weatherData.current.condition.icon} alt="weather icon" />
+        </h1>
+        <p>Temperature: {weatherData.current.temp_c}°</p>
+        <p>Humidity: {weatherData.current.humidity}</p>
+        <p>Wind Speed: {weatherData.current.wind_kph} Km/hr</p>
+      </div>
+    );
+  }
+}
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      activePlace: 0
+    };
+  }
+
   render() {
+    const activePlace = this.state.activePlace;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>React Simple Weather App</Navbar.Brand>
+          </Navbar.Header>
+        </Navbar>
+        <Grid>
+          <Row>
+            <Col md={4} sm={4}>
+              <h3>Select a city</h3>
+              <Nav
+                bsStyle="pills"
+                stacked
+                activeKey={activePlace}
+                onSelect={index => {
+                  this.setState({ activePlace: index });
+                }}
+              >
+                {PLACES.map((place, index) => (
+                  <NavItem key={index} eventKey={index}>
+                    {place.name}
+                  </NavItem>
+                ))}
+              </Nav>
+            </Col>
+            <Col md={8} sm={8}>
+              <WeatherDisplay key={activePlace} zip={PLACES[activePlace].zip} />
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
